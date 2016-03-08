25a9f36d91cffb1c39b43fd06866645b3f14b9c1
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2016-03-08 11:10:17 +0100

    code cleanup: inline & remove method computeBandeauHeaderLinkMyAccount

--- src/main/java/prolongationENT/ComputeBandeau.java
+++ src/main/java/prolongationENT/ComputeBandeau.java
@@ -112,14 +112,9 @@ public class ComputeBandeau {
   	return different_referrer ? 10 : 120; // seconds
     }
 
-    String computeBandeauHeaderLinkMyAccount(HttpServletRequest request, Map<String,AppDTO> validApps) {
-	return file_get_contents(request, "templates/headerLinkMyAccount.html");
-    }
-
     String computeBandeauHeaderLinks(HttpServletRequest request, Ldap.Attrs user, Map<String,AppDTO> validApps) {
 	String template = file_get_contents(request, "templates/headerLinks.html");
-
-	String myAccount = computeBandeauHeaderLinkMyAccount(request, validApps);
+	String myAccount = file_get_contents(request, "templates/headerLinkMyAccount.html");
 
 	String login = user.containsKey("supannAliasLogin") ? user.get("supannAliasLogin").get(0) : user.get("uid").get(0);
 	return String.format(template,
