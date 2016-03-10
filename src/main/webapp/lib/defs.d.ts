459e09835f75a573ba68500adfd7f2bf65bc07ea
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2016-03-10 20:29:25 +0100

    sync defs.d.ts

--- src/main/webapp/lib/defs.d.ts
+++ src/main/webapp/lib/defs.d.ts
@@ -1,35 +1,71 @@
+interface DATA {
+  person: { id: string };
+  bandeauHeader: string;
+  apps: {};
+  layout: {};
+  realUserId: string;
+  canImpersonate: string[];
+}
+
+interface PARAMS {
+  hash: string;
+  is_old: boolean;
+}
+
+interface CONF {
+  bandeau_ENT_url: string;
+  ent_logout_url: string;
+  cas_impersonate: { cookie_name: string, cookie_domain: string };
+  disableLocalStorage: boolean;
+  time_before_checking_browser_cache_is_up_to_date: number;
+}
+
+interface CSS {
+  base: string;
+  desktop: string;
+}
+
+interface bandeau_ENT {
+  quirks: {};
+  current: string;
+  currentAppIds: string[];
+  div_id: string;
+  div_is_uid: boolean;
+  logout: boolean;
+  login: boolean;
+  is_logged: boolean | { fn: (find: any) => boolean };
+  account_links: {};
+  forced_uid: boolean;
+
+  hide_menu: boolean;
+  showSearch: boolean;
+  no_titlebar: boolean;
+  no_footer: boolean;
+
+  onload(DATA: DATA, PARAMS: PARAMS, CONF: CONF): void;
+
+  url: string;
+
+  uid: string;
+  prevHash: string;
+  localStorage_prefix: string;
+  localStorage_js_text_field: string;
+  loadTime: number;
+  notFromLocalStorage: boolean;
+  
+  maybe_loaded: boolean;
+  
+  helpers: typeof h;
+  main: (DATA: DATA, PARAMS: PARAMS) => string;
+  DATA: DATA;
+  CONF: CONF;
+  PARAMS: PARAMS;
+  CSS: CSS;
+}
+
 interface Window {
-  cssToLoadIfInsideIframe: string,
-  bandeau_ENT_onAsyncLogout(): void,
-  bandeau_ENT_detectReload(time): void,
-  bandeau_ENT: {
-    helpers: typeof h
-    quirks: {},
-    current: string,
-    currentAppIds: string[],
-    div_id: string,
-    div_is_uid: boolean,
-    logout: boolean,
-    login: boolean,
-    is_logged: boolean | { fn: (find: any) => boolean },
-    account_links: {},
-    forced_uid: boolean,
-
-    hide_menu: boolean,
-    showSearch: boolean,
-    no_titlebar: boolean,
-    no_footer: boolean,
-
-    onload(DATA: any, PARAMS: any, CONF: any): void,
-
-    url: string,
-
-    uid: string,
-    prevHash: string,
-    localStorage_prefix: string,
-    localStorage_js_text_field: string,
-    js_text: string,
-    loadTime: string,
-    notFromLocalStorage: boolean,
-  }
+  cssToLoadIfInsideIframe: string;
+  bandeau_ENT_onAsyncLogout(): void;
+  bandeau_ENT_detectReload(time): void;
+  bandeau_ENT: bandeau_ENT;
 }
