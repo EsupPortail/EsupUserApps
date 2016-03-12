e8281780af2d81b935d8a0bf64804057a76657ab
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2016-03-12 12:27:16 +0100

    cleanup: inline templates/headerLinks.html

--- src/main/java/prolongationENT/ComputeBandeau.java
+++ src/main/java/prolongationENT/ComputeBandeau.java
@@ -122,21 +122,13 @@ public class ComputeBandeau {
   	return different_referrer ? 10 : 120; // seconds
     }
 
-    String computeBandeauHeaderLinks(HttpServletRequest request, Ldap.Attrs user, Map<String,AppDTO> validApps) {
-	String template = file_get_contents(request, "templates/headerLinks.html");
+    String computeBandeauHeader(HttpServletRequest request, Ldap.Attrs user, AppsDTO userChannels) {
+	String template = file_get_contents(request, "templates/header.html");
 
 	String login = user.containsKey("supannAliasLogin") ? user.get("supannAliasLogin").get(0) : user.get("uid").get(0);
 	return String.format(template,
 			     user.containsKey("displayName") ? user.get("displayName").get(0) : user.get("mail").get(0), 
 			     user.containsKey("displayName") ? user.get("mail").get(0) + " (" + login + ")" : login);
-    }
-
-    String computeBandeauHeader(HttpServletRequest request, Ldap.Attrs user, AppsDTO userChannels) {
-	String template = file_get_contents(request, "templates/header.html");
-
-	String portalPageBarLinks = user != null ? computeBandeauHeaderLinks(request, user, userChannels) : "";
-
-	return String.format(template, portalPageBarLinks);
     }
     
     String get_css_with_absolute_url(HttpServletRequest request, String css_file) {
