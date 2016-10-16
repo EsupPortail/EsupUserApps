96598f6412d4e1adb234c8f3629ff3d2cd7f6d55
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2016-10-16 21:05:34 +0200

    cleanup: make typescript happy

--- src/main/webapp/lib/plugin-delegateAuth.ts
+++ src/main/webapp/lib/plugin-delegateAuth.ts
@@ -2,10 +2,10 @@
     var plugin = {
         onNotLogged: function() {
             if (args.delegateAuth) {
-                var url = pE.CONF.prolongationENT_url + "/login?target=" + encodeURIComponent(document.location);
-                document.location = pE.CONF.cas_login_url + "?service=" + encodeURIComponent(url);
+                var url = pE.CONF.prolongationENT_url + "/login?target=" + encodeURIComponent(document.location.href);
+                document.location.href = pE.CONF.cas_login_url + "?service=" + encodeURIComponent(url);
             }
-        }
+        },
     };
     pE.plugins.push(plugin);   
 })();
