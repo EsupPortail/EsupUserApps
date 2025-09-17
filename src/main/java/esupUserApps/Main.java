b7d008bb514ebcc90e467e77270c6e0524bd24d6
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2025-09-17 16:05:24 +0200

    feat: allow /admin/config-apps.json using Bearer token (GLPI UP1#210004)

--- src/main/java/esupUserApps/Main.java
+++ src/main/java/esupUserApps/Main.java
@@ -100,7 +100,7 @@ public class Main extends HttpServlet {
     }
 
     void show_config_apps(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (get_CAS_userId(request) == null) {
+        if (!hasValidBearerToken(request, conf.shibboleth) && get_CAS_userId(request) == null) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "you must authenticate first");
         } else {
             respond_json(response, objectFieldsToMap(conf, "LAYOUT", "APPS", "GROUPS"));
